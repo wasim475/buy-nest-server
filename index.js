61870d170cce9e0330b7d86502dcc823c1e762bf
wasim475 <wasimhossain575@gmail.com>
2024-08-17 14:42:42 +0600

    deleted default function and comments

--- index.js
+++ index.js
@@ -47,10 +47,7 @@ async function run() {
       });
   });
   
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+   
     console.log("Pinged Your deployment. You  Successfully Connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
